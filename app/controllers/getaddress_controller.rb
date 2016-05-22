f795c3f91e75479138a51b3caaa02109cab5c568
Paul Saperstone <paul@saperstone.com>
2016-05-22 18:56:49 -0400

    Property create bug fixes

--- app/controllers/getaddress_controller.rb
+++ app/controllers/getaddress_controller.rb
@@ -34,16 +34,9 @@ class GetaddressController < ApplicationController
       prop_addr = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[1]/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/span')[0].text
       owner = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[1]/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span')[0].text
       mail_addr = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[1]/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span')[0].text
-      abbr_legal_desc = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[3]/tbody/tr/td[2]/span')[0].text
-      prop_id = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[1]/tbody/tr/td[3]/table/tbody/tr[1]/td[2]/span')[0].text
+      legal_desc = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[3]/tbody/tr/td[2]/span')[0].text
+      prop_acct_num = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[1]/tbody/tr/td[3]/table/tbody/tr[1]/td[2]/span')[0].text
       home_value = browser.all(:xpath, '/html/body/table[2]/tbody/tr/td/table/tbody/tr[1]/td[1]/table[5]/tbody/tr[3]/td[4]/span')[0].text
-
-      # puts owner
-      # puts prop_addr
-      # puts mail_addr
-      # puts abbr_legal_desc
-      # puts prop_id
-      # puts home_value
 
       search_addr = prop_addr.gsub(" ","+").gsub("-","+")
       api_key = 'AIzaSyDa1BWxkgm1n3tljbV-J_6bo3r7jV1UsD4'
@@ -61,27 +54,41 @@ class GetaddressController < ApplicationController
       prop_zip = "#{address_components[7]["long_name"]}"
       prop_county = "#{address_components[4]["long_name"]}"
 
+
+      search_addr = mail_addr.gsub(" ","+").gsub("-","+")
+      api_key = 'AIzaSyDa1BWxkgm1n3tljbV-J_6bo3r7jV1UsD4'
+
+      url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{search_addr}&key=#{api_key}"
+      response = HTTParty.get url
+
+      dom = Nokogiri::HTML(response.body)
+      parsed = JSON.parse(dom)
+      address_components = parsed['results'][0]['address_components']
+
+      mail_str_addr = "#{address_components[0]["long_name"]} #{address_components[1]["long_name"]}"
+      mail_city = "#{address_components[3]["long_name"]}"
+      mail_state = "#{address_components[5]["short_name"]}"
+      mail_zip = "#{address_components[7]["long_name"]}"
+      mail_county = "#{address_components[4]["long_name"]}"
+
+
       Property.create(owner: name,
                       prop_str_addr: prop_str_addr,
                       prop_city: prop_city,
                       prop_state: prop_state,
                       prop_zip: prop_zip,
                       prop_county: prop_county,
+                      mail_str_addr: prop_str_addr,
+                      mail_city: prop_city,
+                      mail_state: prop_state,
+                      mail_zip: prop_zip,
+                      mail_county: prop_county,
+                      home_value: home_value,
+                      legal_desc: legal_desc,
                       document_num: document_num,
+                      prop_acct_num: prop_acct_num,
                       doc_number_lp: doc_number_lp
       )
-
-      # Property.find_by(owner: name).update(prop_str_addr: prop_str_addr)
-      # Property.find_by(owner: name).update(prop_city: prop_city)
-      # Property.find_by(owner: name).update(prop_state: prop_state)
-      # Property.find_by(owner: name).update(prop_zip: prop_zip)
-      # Property.find_by(owner: name).update(prop_county: prop_county)
-
     end
-
-
-# .find(), .find_by()
-# .create()
-
   end
 end
