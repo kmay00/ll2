90fbfd5c6a2345010d57328b1c29bd3fa36023cf
Paul Saperstone <paul@saperstone.com>
2016-05-22 17:41:41 -0400

    seed logic moved to getleads controller

--- app/controllers/getleads_controller.rb
+++ app/controllers/getleads_controller.rb
@@ -1,5 +1,20 @@
 class GetleadsController < ApplicationController
   def index
     #get leads from Civil Court
+    data = CSV.read('broward.csv')
+    #get rid of the header info
+    data.slice!(0)
+
+    data.each do |property|
+      document_num = property[0]
+      owner = property[1]
+      record_date = property[7]
+      doc_number_lp = property[10]
+
+      Property.create(document_num: document_num,
+                      owner: owner,
+                      record_date: record_date,
+                      doc_number_lp: doc_number_lp)
+      end    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-data = CSV.read('broward.csv')
-#get rid of the header info
-data.slice!(0)
-
-data.each do |property|
-  document_num = property[0]
-  owner = property[1]
-  record_date = property[7]
-  doc_number_lp = property[10]
-
-  Property.create(document_num: document_num,
-                  owner: owner,
-                  record_date: record_date,
-                  doc_number_lp: doc_number_lp)
-  end
