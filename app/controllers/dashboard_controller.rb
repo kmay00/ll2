df5f48839f4a515045c59e9539186361945cada9
kmay00 <kmay00@gmail.com>
2016-05-30 14:57:03 -0400

    dashboard nav and font awesome icons

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,7 +16,7 @@ class DashboardController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @purchases.to_csv }
-      format.xls { send_data @purchases.to_csv(col_sep: "\t") }
+      # format.xls { send_data @purchases.to_csv(col_sep: "\t") }
     end
   end
 
